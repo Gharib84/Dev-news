f7dabdd68645818ea23e0b61bb0e443a52b13760
Gharib84e <ITprofessional63@gmail.com>
2025-02-10 22:12:04 +0100

    firebase configuration done for an entire project

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD4BWzgUadRkzCmrdhRlITbcvHpWWgvB60",
+    authDomain: "dev-to-a47b9.firebaseapp.com",
+    projectId: "dev-to-a47b9",
+    storageBucket: "dev-to-a47b9.firebasestorage.app",
+    messagingSenderId: "935189377503",
+    appId: "1:935189377503:web:e271ad780b52856e96d46a",
+    measurementId: "G-0WJWEFZPJ6"
+
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { firebaseConfig } from './environments/environment';
+import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
