1d8de2a8c411f49f0a0cd38827647b17bd727e3f
Gharib84e <ITprofessional63@gmail.com>
2025-02-11 16:24:06 +0100

    fixed firebasse integration with existing app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,8 @@ export const environment = {
     apiKey: "AIzaSyD4BWzgUadRkzCmrdhRlITbcvHpWWgvB60",
     authDomain: "dev-to-a47b9.firebaseapp.com",
     projectId: "dev-to-a47b9",
-    storageBucket: "dev-to-a47b9.firebasestorage.app",
+    storageBucket: "dev-to-a47b9.appspot.com", // Corrected value
     messagingSenderId: "935189377503",
     appId: "1:935189377503:web:e271ad780b52856e96d46a",
     measurementId: "G-0WJWEFZPJ6"
-
 };
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ import { environment } from './environments/environment';
 import { getAnalytics } from 'firebase/analytics';
 import { initializeApp } from 'firebase/app';
 
+
+// Bootstrap Angular application
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
+  // Initialize Firebase
 const app = initializeApp(environment);
-const analytics = getAnalytics(app);
+const analytics = getAnalytics(app)
