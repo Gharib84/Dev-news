781ec335cb195abc7353008be4ebc97e2f106b5b
Gharib84e <ITprofessional63@gmail.com>
2025-02-13 22:47:07 +0100

    create comment prepared to testing with firebase

--- src/app/service/firsebase.service.ts
+++ src/app/service/firsebase.service.ts
@@ -1,15 +1,26 @@
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs';
-import { Firestore, collectionData, collection, getFirestore } from '@angular/fire/firestore';
+import { map, switchMap, from } from 'rxjs';
+import { Firestore, addDoc, collection, getDocs, query } from '@angular/fire/firestore';
+import { Comment } from '../core/interfaces/comment';
 @Injectable({
   providedIn: 'root'
 })
 export class FirsebaseService {
-  private collectioName:string = "comments";
+  private collectioName: string = "comments";
   private fireStore = inject(Firestore);
 
   constructor() { }
 
-  
+  //create a new comment
+
+  // Create a new comment
+  addComment(): Observable<any> {
+    const addCommentPromise = addDoc(collection(this.fireStore, this.collectioName), {
+      name: "Los Angeles",
+      description: "LA is a city"
+    });
+
+    return from(addCommentPromise);
+  }
 }
